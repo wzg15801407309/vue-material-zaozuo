b3754dac3c65dc3fdb6d94fe5fad24c3beabeab7
zhuqin.wei <zhuqin.wei@zhuqinweideMacBook-Pro.local>
2021-04-29 10:56:12 +0800

    vue-material的安装

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+//vue-material 插件的东西
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false;
 
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // 解决 eslint 格式校验
+    lintOnSave : false,
+};
+  
